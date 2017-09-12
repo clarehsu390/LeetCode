c0f4f4f6f15429c92935d93f58af5444025d8345
Clare Hsu <clare.hsu390@gmail.com>
2017-09-12 12:13:25 -0700

    Implement three_sum

--- three_sum.rb
+++ three_sum.rb
@@ -4,19 +4,29 @@ def three_sums(nums)
     nums.sort!
     new_arr = []
     #three pointers -> current, left, right
-    sum = nums[idx]
     idx = 0
+    sum = - nums[idx]
     left = idx + 1
     right = nums.length - 1
+    return [] if nums.select {|el| el >= 0}
     
     while right > left && idx < nums.length
-        if nums[idx] + nums[left] + nums[right] != 0
+        if nums[left] + nums[right] < sum
             left += 1
+        elsif nums[left] + nums[right] > sum
             right -= 1
         elsif nums[idx] + nums[left] + nums[right] == 0
             new_arr << [nums[idx], nums[left], nums[right]]
+            left += 1
+            right -= 1
         end
+        idx += 1
     end
+
+    new_arr
+end
+
+p three_sums([-1, 0, 1, 2, -1, -4])
 
         
 
