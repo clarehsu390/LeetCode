78b682b32dc76f7ff5dd2da9726ac825b09eb80b
Clare Hsu <clare.hsu390@gmail.com>
2017-09-13 14:27:37 -0700

    Refactor remove nth node from end

--- remove_nth_from_end.rb
+++ remove_nth_from_end.rb
@@ -1,5 +1,5 @@
 def remove_nth_from_end(head, n)
-    fakehead = ListNode.new(nil)
+   fakehead = ListNode.new(nil)
     fakehead.next = head
     current = fakehead
     count = 0
@@ -8,21 +8,26 @@ def remove_nth_from_end(head, n)
         current = current.next
         count += 1
     end
-
     nth_index = count - n
+    p nth_index
     current = fakehead
     new_count = 0
     while current.next
-        new_count += 1
+        
 
         if new_count == nth_index
             current.next = current.next.next
         else
         current = current.next
         end
+        new_count += 1
     end
 
-    fakehead.next    
+    fakehead.next
+    
+
+    
 
 
 end
+
