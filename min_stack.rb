8d552b889be2ec5601221497af3478f04d134734
Clare Hsu <clare.hsu390@gmail.com>
2017-09-13 15:36:15 -0700

    implement min stack

---
+++ min_stack.rb
@@ -0,0 +1,52 @@
+class MinStack
+
+=begin
+    initialize your data structure here.
+=end
+    def initialize()
+        @store = []
+    end
+=begin
+    :type x: Integer
+    :rtype: Void
+=end
+    def push(x)
+        @store << x
+        
+    end
+
+
+=begin
+    :rtype: Void
+=end
+    def pop
+        @store.pop
+        
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def top
+        @store.last
+        
+    end
+
+
+=begin
+    :rtype: Integer
+=end
+    def get_min
+        minimum = @store.pop
+        until @store.empty?
+            element = @store.pop
+            if minimum > element
+                minimum = element
+            end
+        end
+        minimum
+    end
+
+
+end
