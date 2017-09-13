047575c68cf18dc3baad597064c2ea554f427d76
Clare Hsu <clare.hsu390@gmail.com>
2017-09-13 14:45:59 -0700

    Modularize remove_nth_node

--- remove_nth_from_end.rb
+++ remove_nth_from_end.rb
@@ -1,31 +1,17 @@
 def remove_nth_from_end(head, n)
-   fakehead = ListNode.new(nil)
-    fakehead.next = head
-    current = fakehead
-    count = 0
-
-    while current.next
-        current = current.next
-        count += 1
-    end
-    nth_index = count - n
-    p nth_index
-    current = fakehead
-    new_count = 0
-    while current.next
-        
-
-        if new_count == nth_index
-            current.next = current.next.next
-        else
-        current = current.next
-        end
-        new_count += 1
-    end
-
-    fakehead.next
+   fast = head
+   slow = head
     
+    while n > 0
+        fast = fast.next
+    end
 
+    while fast.next
+        fast = fast.next
+        slow = slow.next
+    end
+    slow.next = slow.next.next
+    head
     
 
 
