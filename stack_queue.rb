b3f351c11b4bd1ee3cab3c4aa853d78218c94fa0
Clare Hsu <clare.hsu390@gmail.com>
2017-09-13 17:01:46 -0700

    Implement stackqueue

--- stack_queue.rb
+++ stack_queue.rb
@@ -5,8 +5,13 @@ class StackQueue
     end
 
     def enqueue
+        @in_stack.push(val)
     end
 
     def dequeue
+        until @in_stack.empty?
+            @out_stack.push(@in_stack.pop)
+        end
+        @out_stack.pop
     end
 end
