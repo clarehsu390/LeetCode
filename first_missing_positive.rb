3faf001defcf8b8bf30df650bdbcf80d8b522c68
Clare Hsu <clare.hsu390@gmail.com>
2017-09-14 11:47:48 -0700

    Implement first missing positive number'

--- first_missing_positive.rb
+++ first_missing_positive.rb
@@ -1,4 +1,16 @@
 def first_missing_positive(nums)
-    nums.each_with_index do |el, idx|
+    num = 1
+    return 1 if nums.empty?
+    actual_sum = (1..nums.max).to_a.reduce(:+)
+    total = nums.reduce(:+)
+
+    if actual_sum - total > 0
+        return actual_sum - total
+    elsif actual_sum - total == 0
+        return nums.max + 1
         
+    end
 end
+
+p first_missing_positive([1,2,0])
+p first_missing_positive([2])
