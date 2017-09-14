d155163f36fbe55bbae8fd50fa3d276239e9de34
Clare Hsu <clare.hsu390@gmail.com>
2017-09-14 13:41:11 -0700

    Add memoization to climb_stairs

--- climb_stairs.rb
+++ climb_stairs.rb
@@ -11,3 +11,14 @@ def climb_stairs(n)
 end
 
 p climb_stairs(3)
+
+
+#memoization
+def climb_stairs2(n)
+    memo = {1 => 1, 2 => 2}
+    return memo[n] if n <= 2
+    memo[n] = climb_stairs(n-2) + climb_stairs(n-1) unless memo[n]
+    memo[n]
+end
+
+p climb_stairs2(4)
