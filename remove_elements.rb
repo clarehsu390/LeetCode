286d5c14415311e50434a89316ed0465868983a6
Clare Hsu <clare.hsu390@gmail.com>
2017-09-17 14:16:36 -0700

    Add another implementation of remove_elements

--- remove_elements.rb
+++ remove_elements.rb
@@ -6,3 +6,12 @@ def remove_element(nums, val)
 end
 
 p remove_element([1,2,3,3,2], 3)
+
+
+def remove_element2(nums, val)
+    nums.reject {|el| el == val}.length
+end
+
+
+p remove_element2([1,2,3,3,2], 3)
+p remove_element2([3,2,2,3], 3)
