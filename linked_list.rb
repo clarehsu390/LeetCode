ea1e4553fdb467b88f3adfcbc515633d8ff1ed6a
Clare Hsu <clare.hsu390@gmail.com>
2017-11-17 11:21:11 -0800

    implement append node

--- linked_list.rb
+++ linked_list.rb
@@ -1,7 +1,38 @@
 class Node
     attr_accessor :key, :val, :next
-    def initialize(key, val)
+
+    def initialize(key = nil, val = nil)
         @key = key
         @val = val
         @next = nil
     end
+end
+
+class LinkedList
+    include Enumerable
+
+    def initialize
+        @head = Node.new
+        @tail = Node.new
+        @head.next = @tail
+    end
+
+   def append(key, val)
+    new_node = Node.new(key, val)
+    self.each do |node|
+        if node.next == @tail
+            node.next = new_node
+            new_node.next = @tail
+        end
+    end
+    
+   end
+
+   def each
+    current_node = @head.next
+        until current_node == @tail
+            yield current_node
+            current_node = current_node.next
+        end
+    end
+end
