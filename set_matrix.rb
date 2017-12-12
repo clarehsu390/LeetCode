83cdf56e7ec5624c981db9b233fd0279e51d14b6
Clare Hsu <clare.hsu390@gmail.com>
2017-12-12 15:12:47 -0800

    Implement set matrix zeroes

---
+++ set_matrix.rb
@@ -0,0 +1,23 @@
+def set_zeroes(matrix)
+    i = 0
+    j = 0
+(0..matrix.length - 1).each do |i|
+    (0..matrix[0].length - 1).each do |j|
+        if matrix[i][j] == 0
+            (matrix[0].length).times do |m|
+                matrix[i][m] = 0
+            end
+            (matrix.length).times do |n|
+                matrix[n][j] = 0
+            end
+        end
+        j += 1
+    end
+    i += 1
+    end
+    matrix
+            
+end
+
+p set_zeroes([[1,0,3],[0,4,5]])
+p set_zeroes([[1], [0]])
