1665f0302aca73ac4342815dea321c9c47773f67
Clare Hsu <clare.hsu390@gmail.com>
2017-12-14 14:51:59 -0800

    Rotate image

---
+++ rotate_image.rb
@@ -0,0 +1,22 @@
+def rotate(matrix)
+    matrix.reverse!
+    length = matrix[0].length
+    (0..matrix.length - 1).each do |i|
+        (i+1..matrix[i].length - 1).each do |j|
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+        end
+    end
+    matrix
+end
+
+p rotate([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+  ]) == [
+    [7,4,1],
+    [8,5,2],
+    [9,6,3]
+  ]
