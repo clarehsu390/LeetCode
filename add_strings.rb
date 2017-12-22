f24e41615b0934358c44ffa7554e4d3cf9d1d9fe
Clare Hsu <clare.hsu390@gmail.com>
2017-12-21 16:32:03 -0800

    Implement add strings

---
+++ add_strings.rb
@@ -0,0 +1,13 @@
+def add_strings(num1, num2)
+    i = 0
+    result = 0
+    num1 = num1.split
+    num2 = num2.split
+    until num1.empty? && num2.empty?
+       result += ((num1.pop.to_i + num2.pop.to_i) * (10 ** i))
+       i += 1
+    end
+    result
+end
+
+p add_strings("521", "10")
