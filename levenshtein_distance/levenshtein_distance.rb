ac767612f30e974f99dfe768b0b978e9cd053295
Clare Hsu <clare.hsu390@gmail.com>
2018-03-19 16:25:56 -0700

    implement basic levenshtein distance

--- levenshtein_distance/levenshtein_distance.rb
+++ levenshtein_distance/levenshtein_distance.rb
@@ -5,20 +5,35 @@ OPERATIONS = [
     "anagram"
 ]
 def find_lowest_cost(costs, word1, word2)
-    costs_hash = {}
-    (0..3).each do |i|
-        operation = OPERATIONS[i]
-        costs_hash[operation] = costs[i]
-    end
+    # costs_hash = {}
+    # (0..3).each do |i|
+    #     operation = OPERATIONS[i]
+    #     costs_hash[operation] = costs[i]
+    # end
     length1 = word1.length
     length2 = word2.length
     return length1 if length2 == 0
     return length2 if length1 == 0
-    result = Array.new(length1)
-    (0..length2).each do |i|
-        result[i] = []
+    result = Array.new(length1+1) {Array.new(length2+1)}
+    (0..length1).each {|i| result[i][0] = i} #intialize first row to 0..length1
+    (0..length2).each {|j| result[0][j] = j} #initialize first column to 0..length2
+    (1..length2).each do |j| #examine each ch of word1 from 1..length1
+        (1..length1).each do |i| #examine each ch of word2 from 1..length2
+            if word1[i-1] == word2[j-1]
+                result[i][j] = result[i-1][j-1]
+            else
+                result[i][j] = [
+                    result[i-1][j] + 1,
+                    result[i][j-1] + 1,
+                    result[i-1][j-1] + 1
+            ].min
+        end
     end
-    result
+end
+result[length1][length2]
+
+
+   
         
 
 end
