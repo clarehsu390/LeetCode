bde9510a09e1d9516d8b39ee12b141479d60e74a
Clare Hsu <clare.hsu390@gmail.com>
2018-03-19 17:38:27 -0700

    implement in order traversal iteratively

--- in_order_traversal.rb
+++ in_order_traversal.rb
@@ -6,3 +6,30 @@ def in_order_traversal(root)
   result.concat(in_order_traversal(root.right))
   result
 end
+
+def iterative_iot(root)
+  #declare a queue
+  arr = []
+  stack = Queue.new
+  current_node = root
+  while (!current_node.nil? || !stack.empty?)
+    while !current_node.nil?
+      stack << current_node
+      current_node = root.left
+    end
+    current_node = stack.pop
+    arr << current_node.val
+    current_node = current_node.right
+  end
+  arr
+
+end
+
+def pre_order_traversal(root)
+  return [] if root.nil?
+  result = []
+  result << root.val
+  result.concat(pre_order_traversal(root.left))
+  result.concat(pre_order_traversal(root.right))
+  result
+end
--- levenshtein_distance/levenshtein_distance.rb
+++ levenshtein_distance/levenshtein_distance.rb
@@ -10,6 +10,7 @@ def find_lowest_cost(costs, word1, word2)
     #     operation = OPERATIONS[i]
     #     costs_hash[operation] = costs[i]
     # end
+    raise 'Enter valid inputs' unless word1.is_a?(String) && word2.is_a?(String)
     length1 = word1.length
     length2 = word2.length
     return length1 if length2 == 0
@@ -20,17 +21,19 @@ def find_lowest_cost(costs, word1, word2)
     (1..length2).each do |j| #examine each ch of word1 from 1..length1
         (1..length1).each do |i| #examine each ch of word2 from 1..length2
             if word1[i-1] == word2[j-1]
-                result[i][j] = result[i-1][j-1]
+                result[i][j] = result[i-1][j-1] #free
             else
                 result[i][j] = [
-                    result[i-1][j] + 1,
-                    result[i][j-1] + 1,
-                    result[i-1][j-1] + 1
+                    result[i-1][j] + costs[0],
+                    result[i][j-1] + costs[1],
+                    result[i-1][j-1] + costs[2],
+                    # costs[3]
             ].min
         end
     end
 end
-result[length1][length2]
+p result
+result[length1][length2] > length1 ? -1 : result[length1][length2]
 
 
    
@@ -38,4 +41,6 @@ result[length1][length2]
 
 end
 
-p find_lowest_cost([1,3,1,5], 'HEALTH', 'HANDS');
+p find_lowest_cost([1,3,1,5], 'HEALTH', 'HANDS')
+# p find_lowest_cost([1,9,1,3], 'TEAM', 'MATE')
+# p find_lowest_cost([7,1,5,2], 'OPHTHALMOLOGY', 'GLASSES')
