3c71d38e619b81d6b3e770f151a1e497a4c2c652
Clare Hsu <clare.hsu390@gmail.com>
2018-03-19 21:38:37 -0700

    edit bug in path sum

--- has_path_sum.rb
+++ has_path_sum.rb
@@ -1,8 +1,6 @@
 def has_path_sum(root, sum)
     return false if root.nil?
     return true if root.val == sum && root.left.nil? && root.right.nil?
-    right_sum = sum - root.right.val
-    left_sum = sum - root.left.val
-    has_path_sum(root.left, left_sum) || has_path_sumr(root.right, right_sum)
+    has_path_sum(root.left, sum - root.val) || has_path_sum(root.right, sum - root.val)
 end
 
--- max_depth.rb
+++ max_depth.rb
@@ -1,6 +1,7 @@
 def max_depth(root)
     return 0 if root.nil?
-    left_depth = max_depth(root.left)
+    #keep traversing until we reach the last leaf
+    left_depth = max_depth(root.left) 
     right_depth = max_depth(root.right)
-    left_depth > right_depth ? left_depth + 1 : right_depth + 1
+    left_depth > right_depth ? left_depth + 1 : right_depth + 1 #include the current leaf we're on
 end
