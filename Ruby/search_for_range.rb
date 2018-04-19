b5dc0375444b67e84a5005a37515f1131be10b16
Clare Hsu <clare.hsu390@gmail.com>
2018-04-18 21:25:25 -0700

    Optimize search for range

--- Ruby/search_for_range.rb
+++ Ruby/search_for_range.rb
@@ -1,3 +1,4 @@
+#O(n)
 def search_range(nums, target)
     start = 0
     finish = 0
@@ -14,4 +15,29 @@ def search_range(nums, target)
     [start, finish]
 end
 
-p search_range([5,7,7,8,8,10], 6)
+#O(log n)
+
+def search_range2(nums, target)
+    left = 0
+    right = nums.length - 1
+    while left <= right
+        mid = left + (right - left)/2
+        if nums[mid] == target #if this is target -- check for the range
+            if nums[left] == target && nums[right] == target
+                return [left, right]
+            elsif nums[left] != target
+                left += 1
+            else
+                right -= 1
+            end
+        elsif nums[mid] < target
+            left = mid + 1 #check the next element
+        else 
+            right = mid - 1 #check the element before
+        end
+    end
+    [-1, -1]
+end
+
+
+p search_range2([5,7,7,8,8,10], 8)
