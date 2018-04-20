c07833c839641ce08ecd13365e36dec7847adcf7
Clare Hsu <clare.hsu390@gmail.com>
2018-04-20 16:34:53 -0700

    Implement cycle detection without extra space

---
+++ Ruby/detect_cycle.rb
@@ -0,0 +1,32 @@
+class Node
+    attr_accessor :val
+    def intialize(val)
+        @val = val
+        @next = nil
+    end
+end
+
+def detect_cycle(node) #O(n) space, O(n) time
+    seen_nodes = {}
+    while node.next
+        if seen_nodes[node.val]
+            return node
+        else
+            seen_nodes[node.val] = true
+        end
+    end
+    null
+end
+
+def detect_cycle2(node)
+    fast_node = node.next.next
+    slow_node = node
+    while fast_node.next && slow_node
+        if fast_node == slow_node
+            return true
+        end
+        slow_node = slow_node.next
+        fast_node = fast_node.next
+    end
+    false
+end
---
+++ Ruby/multiply_strings.rb
@@ -0,0 +1,7 @@
+def multiply(num1, num2)
+    length1 = num1.length
+    length2 = num2.length
+    pos = []
+
+    
+end
